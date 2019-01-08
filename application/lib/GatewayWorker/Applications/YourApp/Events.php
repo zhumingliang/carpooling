120fd5d4bb84b7cee801df80c1d2b137df68c174
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-01-09 00:09:38 +0800

    项目初始化

--- application/lib/GatewayWorker/Applications/YourApp/Events.php
+++ application/lib/GatewayWorker/Applications/YourApp/Events.php
@@ -57,7 +57,7 @@ class Events
     public static function onMessage($client_id, $message)
     {
         $info = json_decode($message);
-        $type = $info->typ;
+        $type = $info->type;
         if ($type == "bind") {
             $u_id = $info->u_id;
             self::saveBind($client_id, $u_id);
